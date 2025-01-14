a35c98160a58922cef0052e2c21d6d81fe6e045f
Abhijeet thakur <141738911+Abhusicx@users.noreply.github.com>
2025-01-14 23:49:14 +0530

    NotificationSent

---
+++ NotificationClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeNotificationSystem
+{
+    public abstract class NotificationthroughClass
+    {
+        public abstract void Notificationtype(string message);
+
+        public NotificationthroughClass()
+        {
+        
+            Console.WriteLine("welcome to our system");
+        }
+
+        public void NewPolicyAnnouncement()
+        {
+            
+            Console.WriteLine("new policy has been announced!" );
+        }
+
+        public void EventReminder()
+        {
+            Console.WriteLine("New Event has been announced!");
+        }
+
+        public void SystemAlert()
+        {
+            Console.WriteLine("new Alert has been announced!");
+        }
+    }
+
+    public class Email : NotificationthroughClass
+    {
+        public override void Notificationtype(string message)
+        {
+            Console.WriteLine("notification through email");
+        }
+    }
+
+    public class SMS : NotificationthroughClass
+    {
+        public override void Notificationtype(string message)
+        {
+            Console.WriteLine("notification through SMS");
+        }
+    }
+
+    public class PushNotification : NotificationthroughClass
+    {
+        public override void Notificationtype(string message)
+        {
+            Console.WriteLine("notification through PushNotification");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿
+using System.Runtime.CompilerServices;
+using EmployeeNotificationSystem;
+
+namespace EmployeeNotificationSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("for abhijeet");
+            NotificationthroughClass Abhijeet = getNotification("Email");
+            Abhijeet.Notificationtype("Abhijeet");
+            Abhijeet.EventReminder();
+            Abhijeet.SystemAlert();
+            Abhijeet.NewPolicyAnnouncement();
+            Console.WriteLine();
+
+            Console.WriteLine("for parth");
+            NotificationthroughClass Parth = getNotification("SMS");
+            Parth.Notificationtype("parth");
+            Parth.EventReminder();
+            Parth.SystemAlert();
+            Parth.NewPolicyAnnouncement();
+            Console.WriteLine();
+
+            Console.WriteLine("for Chandragupt");
+            NotificationthroughClass Chandragupt = getNotification("Push");
+            Parth.Notificationtype("parth");
+            Chandragupt.EventReminder();
+            Chandragupt.SystemAlert();
+            Chandragupt.NewPolicyAnnouncement();
+        }
+
+        private static NotificationthroughClass getNotification(string v)
+        {
+            if (v == "Email")
+            {
+                return new Email();
+            }
+            else if (v == "SMS")
+            {
+                return new SMS();
+            }
+            else if (v == "Push")
+            {
+                return new PushNotification();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
